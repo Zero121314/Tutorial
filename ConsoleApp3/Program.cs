5469006b8cdf09f40095ce20f23bba9dbb9605e1
HUNG3 NGUYEN VAN <nvhung3@NVHUNG3-VDI>
2022-08-29 09:53:05 +0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadStateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating and initializing threads Unstarted state
+            Thread thread1 = new Thread(SomeMethod);
+
+            Console.WriteLine($"ThreadState: {thread1.ThreadState}");
+
+            // Running state
+            thread1.Start();
+            Console.WriteLine($"ThreadState: {thread1.ThreadState}");
+
+
+
+            // thread1 is in Abort state
+            thread1.Resume();
+            Console.WriteLine($"ThreadState: {thread1.ThreadState}");
+
+            Console.ReadKey();
+        }
+
+        public static void SomeMethod()
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("SomeMethod.....");
+            }
+        }
+    }
+}
